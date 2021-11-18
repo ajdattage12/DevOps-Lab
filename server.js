66caf3cc96b6ed4fabd4bd979da07b47daf15148
Ariell Dattage <ariell.dattage@Ariells-MBP.localdomain>
2021-11-18 13:32:33 -0700

    add fake function

--- server.js
+++ server.js
@@ -20,6 +20,18 @@ app.get('/', function(req, res) {
     res.sendFile(path.join(__dirname, "/public/index.html"))
 })
 
+app.post('/', function(req, res){
+    try {
+    nonExistentFunction();
+  } catch (error) {
+    console.error(error);
+    // expected output: ReferenceError: nonExistentFunction is not defined
+    // Note - error messages will vary depending on browser
+  }
+    rollbar.info("This is the fake function error");
+    res.status(400).send("Opps! That is not allowed.")
+})
+
 app.use("/", express.static(path.join(__dirname, "/public")));
 
 app.use('/css', express.static(path.join(__dirname, "/styles.css")))
