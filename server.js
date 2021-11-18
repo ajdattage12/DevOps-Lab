829227fa602afbd3ec1ae5aec5ac2cd59053bd80
Ariell Dattage <ariell.dattage@Ariells-MBP.localdomain>
2021-11-18 14:26:58 -0700

    Here we go again

--- server.js
+++ server.js
@@ -25,8 +25,6 @@ app.post('/', function(req, res){
     nonExistentFunction();
   } catch (error) {
     console.error(error);
-    // expected output: ReferenceError: nonExistentFunction is not defined
-    // Note - error messages will vary depending on browser
   }
     rollbar.info("This is the fake function error");
     res.status(400).send("Opps! That is not allowed.")
